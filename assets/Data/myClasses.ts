d9ea0a0a8376f031ce668c745fd20439d14b11b2
Neil Froggatt <neilfr@live.com>
2019-07-18 11:47:54 -0400

    Add files via upload

---
+++ assets/Data/myClasses.ts
@@ -0,0 +1,16 @@
+import { ClassInfo } from "../../app/classInfo";
+
+export const MYCLASSES: ClassInfo[] = [
+  {
+    cname: "syst2444",
+    room: "g202",
+    dow: "thurs",
+    tod: "9am"
+  },
+  {
+    cname: "syst54321",
+    room: "b333",
+    dow: "friday",
+    tod: "2pm"
+  }
+];
